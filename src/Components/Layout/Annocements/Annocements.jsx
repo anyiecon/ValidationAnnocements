071c06887308efcd972f9b11dd6e33eca14f924e
Daniel Pachon <danielpachon322@gmail.com>
2022-05-09 14:36:58 -0500

    FilterAnnocements

--- src/Components/Layout/Annocements/Annocements.jsx
+++ src/Components/Layout/Annocements/Annocements.jsx
@@ -1,30 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { UIButtons } from "../../UI/UIButtons/UIButtons";
 import { UICards } from "../../UI/UICards/UICardsAnnocements";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export const Annocements = () => {
     
   const [products, setProducts] = useState([]);
+ const [ serachProducts , setSerachProducts ] = useState([])
+  const [ search , setSearch] = useState("")
   
   const uidUsers = localStorage.getItem('uiduser')
   
   console.log(uidUsers);
 
   const URL = `https://fullmarket-provitional-backend.herokuapp.com/getmyproducts/${uidUsers}`
-  //const URL = "https://backend-fullmarket-py.herokuapp.com/getallproducts";
 
-  
   const mostrar = async () => {
     await fetch(URL)
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) =>{
+        setProducts(data)
+        setSerachProducts(data)
+      })
   };
   useEffect(() => {
     mostrar();
   }, []);
   console.log(products);
 
+  const handleOnchange=(e)=>{
+      console.log(e.target.value);
+      setSearch(e.target.value)
+      filterP(e.target.value)
+  }
+
+   const filterP = (toserachs)=>{
+     var resultsFilter= serachProducts.filter((elemento)=>{
+       if(elemento.name.toString().toLowerCase().includes(toserachs.toLowerCase()) 
+       ||elemento.type.toString().toLowerCase().includes(toserachs.toLowerCase()) 
+       ){
+         return elemento
+       }
+     });
+     setProducts(resultsFilter)
+   }
+
   return (
+    <div>
     <main className="main-products">
     <div className="navbarAnnocements">
      <UIButtons 
@@ -37,6 +60,12 @@ export const Annocements = () => {
         Buttons="btn-Apply"
         nameButtons="Regalar Producto"
      ></UIButtons>
+     <div className="search ">
+        <input value={search} type='text' onChange={handleOnchange}></input>
+        <button className="btn">
+          <FontAwesomeIcon icon={faSearch}/>
+        </button>
+      </div>
      {/* <input placeholder="Busca tus productos"></input> */}
      </div>
       {products.map((element) => (
@@ -54,6 +83,7 @@ export const Annocements = () => {
         />
       ))}
     </main>
+    </div>
   );
 }
 
